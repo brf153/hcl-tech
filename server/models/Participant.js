42bd65b58270d11e65f8eb5c2ce837440c6fee9a
brf153 <153hsb@gmail.com>
2025-02-09 19:24:42 +0530

    fix schema
    Signed-off-by: brf153 <153hsb@gmail.com>

--- server/models/Participant.js
+++ server/models/Participant.js
@@ -1,12 +1,54 @@
 const mongoose = require("mongoose");
 
 const participantSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  weight: Number,
-  heartRate: Number,
-  consentGiven: Boolean,
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  age: { type: Number },
+  gender: { type: String, enum: ["Male", "Female", "Non-Binary", "Other"], required: true },
+  maritalStatus: { 
+    type: String, 
+    enum: ["Single", "Married", "Divorced", "Widowed", "Other"], 
+    required: true 
+  },
+
+  address: {
+    street: { type: String },
+    city: { type: String },
+    state: { type: String },
+    zipCode: { type: String },
+    country: { type: String }
+  },
+  phoneNumber: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+
+  employmentStatus: { 
+    type: String, 
+    enum: ["Employed", "Unemployed", "Retired", "Student", "Other"] 
+  },
+  occupation: { type: String },
+  educationLevel: { 
+    type: String, 
+    enum: ["No Formal Education", "High School", "Bachelor’s Degree", "Master’s Degree", "Doctorate", "Other"] 
+  },
+
+  raceEthnicity: { 
+    type: String, 
+    enum: ["White", "Black/African American", "Asian", "Hispanic/Latino", "Native American", "Pacific Islander", "Mixed", "Other"] 
+  },
+  nationality: { type: String },
+  primaryLanguages: [{ type: String }],
+
+  healthStatus: { type: String },
+  chronicConditions: [{ type: String }],
+  disabilities: [{ type: String }],
+  smoking: { type: Boolean },
+  alcoholUse: { type: Boolean },
+  drugUse: { type: Boolean },
+  physicalActivityLevel: { type: String },
+
   visits: [{ type: mongoose.Schema.Types.ObjectId, ref: "Visit" }],
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Participant", participantSchema);
